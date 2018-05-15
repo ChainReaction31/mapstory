33109d0507a397ffc5162772c8cee7caa1105f5d
Nick Garay <nicolasgaray@icloud.com>
2018-05-15 14:28:09 -0500

    Compilable Fields

--- mapstory/apps/opensensorhub/fields.py
+++ mapstory/apps/opensensorhub/fields.py
@@ -4,10 +4,10 @@ import json
 
 
 class ArrayField(models.Field):
-    description = "A container for data to be used as arrays"
+    description = 'A container for data to be used as arrays'
 
-#    def __init__(self, *args, **kwargs):
-#        super(self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super(ArrayField, self).__init__(*args, **kwargs)
 
     def deconstruct(self):
         name, path, args, kwargs = super(ArrayField, self).deconstruct()
@@ -24,11 +24,11 @@ class ArrayField(models.Field):
 
 
 class RgbField(ArrayField):
-    description = "An array containing the integer values for RGB color definition"
+    description = 'An array containing the integer values for RGB color definition'
 
     def __init__(self, *args, **kwargs):
         kwargs['max_length'] = 3
-        super(RgbField, self).__init__(self, *args, **kwargs)
+        super(RgbField, self).__init__(*args, **kwargs)
 
     def deconstruct(self):
         name, path, args, kwargs = super(RgbField, self).deconstruct()
@@ -68,11 +68,11 @@ class RgbField(ArrayField):
 
 
 class ThresholdField(ArrayField):
-    description = "Container for value pairs as (min, max)"
+    description = 'Container for value pairs as (min, max)'
 
     def __init__(self, *args, **kwargs):
         kwargs['max_length'] = 2
-        super(ThresholdField, self).__init__(self, *args, **kwargs)
+        super(ThresholdField, self).__init__(*args, **kwargs)
 
     def deconstruct(self):
         name, path, args, kwargs = super(ThresholdField, self).deconstruct()
@@ -93,10 +93,10 @@ class ThresholdField(ArrayField):
 
 
 class IntThresholdField(ThresholdField):
-    description = "Container for integer value pairs as (min, max)"
+    description = 'Container for integer value pairs as (min, max)'
 
     def __init__(self, *args, **kwargs):
-        super(IntThresholdField, self).__init__(self, *args, **kwargs)
+        super(IntThresholdField, self).__init__(*args, **kwargs)
 
     def deconstruct(self):
         name, path, args, kwargs = super(IntThresholdField, self).deconstruct()
@@ -135,10 +135,10 @@ class IntThresholdField(ThresholdField):
 
 
 class FloatThresholdField(ThresholdField):
-    description = "Container for floating point precision value pairs as (min, max)"
+    description = 'Container for floating point precision value pairs as (min, max)'
 
     def __init__(self, *args, **kwargs):
-        super(FloatThresholdField, self).__init__(self, *args, **kwargs)
+        super(FloatThresholdField, self).__init__(*args, **kwargs)
 
     def deconstruct(self):
         name, path, args, kwargs = super(FloatThresholdField, self).deconstruct()
@@ -177,10 +177,10 @@ class FloatThresholdField(ThresholdField):
 
 
 class ThresholdArrayField(ArrayField):
-    description = "An array containing threshold values"
+    description = 'An array containing threshold values'
 
-    def __init__(self, array_field, *args, **kwargs):
-        super(ThresholdArrayField, self).__init__(self, *args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super(ThresholdArrayField, self).__init__(*args, **kwargs)
 
     def deconstruct(self):
         name, path, args, kwargs = super(ThresholdArrayField, self).deconstruct()
